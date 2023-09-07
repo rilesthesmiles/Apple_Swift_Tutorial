1bc5d5c11587b320657ea7118079eaf671cc7f92
Riley_Marshall <rileyrmarshall1@gmail.com>
2023-09-07 08:35:40 -0600

    Initial Commit

---
+++ Apple_Swift_Tutorial/Apple_Swift_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Swift_TutorialApp.swift
+//  Apple_Swift_Tutorial
+//
+//  Created by Riley Marshall on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Swift_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple_Swift_Tutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apple_Swift_Tutorial
+//
+//  Created by Riley Marshall on 9/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
